a047a8b0767c5b14b6fc79c9bac2c8c590ae6390
Angela Li <alidartmouth20@gmail.com>
2017-11-05 23:23:45 -0500

    fonts updated

--- js/scripts.js
+++ js/scripts.js
@@ -25,21 +25,75 @@ $(document).ready(function() {
             'slow');
     })
 
-    // var visible = true;
-    //
-    // $("#rando").click(function(){
-    //   if(visible == true){
-    //     $("#home").css("visibility", "hidden");
-    //     visible = false;
-    //   }
-    //   else {
-    //     $("#home").css("visibility", "visible");
-    //     visible = true;
-    //   }
-    // })
+    $(".child").hide();
+    $("#home").show(100);
 
-    $("#rando").click(function(){
-        $("#dartmouth").toggle(100);
+    clicked = false;
+
+    $(".dot").hover(
+      function(){
+        $(this).fadeTo(1, 1);
+        clicked = false;
+      },
+      function(){
+        if (clicked == false){
+          $(this).fadeTo(1, 0);
+        }
+      }
+    );
+
+    $(".dot").click(function(){
+      $(".dot").fadeTo(1, 0);
+      $(this).fadeTo(1, 1);
+      clicked = true;
+    });
+
+    $("#num1").hover(
+      function(){
+        $("#home").show(100);
+      },
+      function(){
+        $("#home").hide(100);
+    });
+
+    $("#num2").hover(
+      function(){
+        $("#dartmouth").show(100);
+      },
+      function(){
+        $("#dartmouth").hide(100);
+    });
+
+    $("#num3").hover(
+      function(){
+        $("#north-carolina").show(100);
+      },
+      function(){
+        $("#north-carolina").hide(100);
+    });
+
+    $("#num4").hover(
+      function(){
+        $("#acadia").show(100);
+      },
+      function(){
+        $("#acadia").hide(100);
+    });
+
+    $("#num5").hover(
+      function(){
+        $("#moosilauke").show(100);
+      },
+      function(){
+        $("#moosilauke").hide(100);
+    });
+
+    $("#num6").hover(
+      function(){
+        $("#pickledish").show(100);
+      },
+      function(){
+        $("#pickledish").hide(100);
     });
 
 });
